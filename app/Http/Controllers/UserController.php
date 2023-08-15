7e6774c1abfd72074cd3ad741fcb05337dc272fa
cecilsuzuki <xxxkiosk.engineer@gmail.com>
2023-08-15 19:55:54 +0900

    会員管理API群のエンドポイント定義

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    public function userSignIn(): JsonResponse
+    {
+        // TODO：サインインAPIの実装
+        return new JsonResponse();
+    }
+
+    public function userSignUp(): JsonResponse
+    {
+        // TODO：サインアップAPIの実装
+        return new JsonResponse();
+    }
+}
